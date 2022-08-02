c5ab85720f32d20531af0365b9e4e1011201b4ac
Jason Kartz <jasonkartz@gmail.com>
2022-08-01 22:36:19 -0500

    working on the gallery

--- scripts.js
+++ scripts.js
@@ -3,6 +3,34 @@ const closeMenu = document.getElementById("close-menu");
 const nav = document.getElementById("nav");
 const darkenBg = document.getElementById("darken");
 
+const btnLeftMobile = document.getElementById("btn-left-mobile");
+const btnRightMobile = document.getElementById("btn-right-mobile");
+const btnLeftDesktop = document.getElementById("btn-left-desktop");
+const btnRightDesktop = document.getElementById("btn-right-desktop");
+
+const infoText1 = document.getElementById("info-text-1");
+const infoText2 = document.getElementById("info-text-2");
+const infoText3 = document.getElementById("info-text-3");
+
+const infoArr = [infoText1, infoText2, infoText3];
+
+let count = 0;
+
+function incrementInfoText() {
+  infoArr[count].style.display = "none";
+  if (count < infoArr.length) {
+    count++;
+    infoArr[count].style.display = "block";
+  } else {
+    count = 0;
+    infoArr[count].style.display = "block";
+  }
+}
+
+btnRightDesktop.addEventListener("click", incrementInfoText);
+
+function decrementInfoText() {}
+
 function displayMenu() {
   nav.style.visibility = "visible";
   nav.style.opacity = 1;
