39dc1977d1c57a09983e6087f4d1cd24ddf1a561
Jason Kartz <jasonkartz@gmail.com>
2022-08-02 18:36:23 -0500

    finished gallery change & screen resize

--- scripts.js
+++ scripts.js
@@ -1,12 +1,16 @@
+//Menu display
 const openMenu = document.getElementById("open-menu");
 const closeMenu = document.getElementById("close-menu");
 const nav = document.getElementById("nav");
 const darkenBg = document.getElementById("darken");
 
+//Gallery/info display
 const btnLeftMobile = document.getElementById("btn-left-mobile");
 const btnRightMobile = document.getElementById("btn-right-mobile");
 const btnLeftDesktop = document.getElementById("btn-left-desktop");
 const btnRightDesktop = document.getElementById("btn-right-desktop");
+
+const header = document.getElementById("header");
 
 const infoText1 = document.getElementById("info-text-1");
 const infoText2 = document.getElementById("info-text-2");
@@ -15,21 +19,6 @@ const infoText3 = document.getElementById("info-text-3");
 const infoArr = [infoText1, infoText2, infoText3];
 
 let count = 0;
-
-function incrementInfoText() {
-  infoArr[count].style.display = "none";
-  if (count < infoArr.length) {
-    count++;
-    infoArr[count].style.display = "block";
-  } else {
-    count = 0;
-    infoArr[count].style.display = "block";
-  }
-}
-
-btnRightDesktop.addEventListener("click", incrementInfoText);
-
-function decrementInfoText() {}
 
 function displayMenu() {
   nav.style.visibility = "visible";
@@ -45,6 +34,53 @@ function hideMenu() {
   darkenBg.style.opacity = 0;
 }
 
+function contentChange() {
+  header.style.background = `url("./images/${
+    window.screen.width >= 450 ? "desktop" : "mobile"
+  }-image-hero-${count + 1}.jpg") no-repeat`;
+  header.style.backgroundSize = "cover";
+  header.style.backgroundPosition = "left";
+  infoArr[count].style.display = "block";
+}
+
+function incrementInfoText() {
+  infoArr[count].style.display = "none";
+  if (count === infoArr.length - 1) {
+    count = 0;
+    contentChange();
+  } else {
+    count++;
+    contentChange();
+  }
+}
+
+function decrementInfoText() {
+  infoArr[count].style.display = "none";
+  if (count === 0) {
+    count = infoArr.length - 1;
+    contentChange();
+  } else {
+    count--;
+    contentChange();
+  }
+}
+
+window.onload = contentChange();
+window.onresize = () => {
+  contentChange();
+  if (window.screen.width > 999) {
+    displayMenu();
+  } else {
+    hideMenu();
+  }
+};
+
 openMenu.addEventListener("click", displayMenu);
 closeMenu.addEventListener("click", hideMenu);
 darkenBg.addEventListener("click", hideMenu);
+
+btnRightDesktop.addEventListener("click", incrementInfoText);
+btnRightMobile.addEventListener("click", incrementInfoText);
+
+btnLeftDesktop.addEventListener("click", decrementInfoText);
+btnLeftMobile.addEventListener("click", decrementInfoText);
